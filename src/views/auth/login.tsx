a22b71b5299415eefd9784ec42b3c192457e703f
muhammadaziz01-dev <akbarovmuhammadaziz050@gmail.com>
2025-03-05 18:31:09 +0600

    auth page style end

--- src/views/auth/login.tsx
+++ src/views/auth/login.tsx
@@ -1,9 +1,37 @@
-
-
+import "./style.scss"
 function Index() {
   return (
-    <div>login</div>
-  )
+    <div className="auth">
+      <form className="auth--form" action="#">
+        <p className="auth--form-title">Login to your account</p>
+        <label htmlFor="username" className="auth--form--label">
+          <p className="auth--form--label-title">Username</p>
+          <input
+           id="username"
+            className="auth--form--label-input"
+            type="text"
+            placeholder="Username"
+          />
+        </label>
+        <label htmlFor="phone" className="auth--form--label">
+        <p className="auth--form--label-title">Phone number</p>
+          <input
+          id="phone"
+            className="auth--form--label-input"
+            type="text"
+            placeholder="+998 (__) (___) (__) (__)"
+          />
+        </label>
+        
+        <button className="auth--form-btn" type="submit">
+        Login now
+        </button>
+        <p className="auth--form--titleButton">
+          Don't have an account ?<span>Sign up</span>
+        </p>
+      </form>
+    </div>
+  );
 }
 
-export default Index
+export default Index;
